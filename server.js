de33320b81f07a8cdfe23e5d16f746c3bbb83411
Liz Masoudi <lizmasoudi@Lizs-MacBook-Pro-2.local>
2017-10-14 12:42:27 -0600

    heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = 3000;
-// Sets up the Express app to handle data parsing
+// =============================================================
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
